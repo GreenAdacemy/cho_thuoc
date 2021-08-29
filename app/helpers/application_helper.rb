e2e95e3963796da342f66e054effc0275fb75696
Phuong Ngo <ngohoai.phuong@gmail.com>
2021-08-29 17:10:21 +0700

    add slide language

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,29 +1,33 @@
 module ApplicationHelper
-    def preload_image
-        asset_pack_path 'media/images/loading.gif'
+  def preload_image
+    asset_pack_path 'media/images/loading.gif'
+  end
+  
+  def load_image (obj)
+    return obj if obj.image.is_a?(String)
+    return default_image unless obj.image.attached?
+    if ActiveStorage::Blob.service.class.name.include?('Disk')
+      url_for(obj.image)
     end
-    def load_image (obj)
-        return obj if obj.image.is_a?(String)
-        return default_image unless obj.image.attached?
-        if ActiveStorage::Blob.service.class.name.include?('Disk')
-            url_for(obj.image)
-        end
+  end
 
-    end
+  def default_image
+    asset_pack_path 'media/images/No_img.jpg'
+  end
+  
+  def fecth_product
+    @products=Product.all
+  end
+  
+  def sexes
+    Profile::sexes.map {|v,k| [k, v]}
+  end
 
-    def default_image
-        asset_pack_path 'media/images/No_img.jpg'
-    end
-    
-    def fecth_product
-        @products=Product.all
-    end
-    
-    def sexes
-      Profile::sexes.map {|v,k| [k, v]}
-    end
+  def fetch_data(json_file = nil)
+    File.exists?(json_file) ? JSON.parse(File.read(json_file)) : []
+  end
 
-    def fetch_data(json_file = nil)
-      File.exists?(json_file) ? JSON.parse(File.read(json_file)) : []
-    end
+  def check_current_lang(lang)
+    I18n.locale.to_sym == lang.to_sym
+  end
 end
